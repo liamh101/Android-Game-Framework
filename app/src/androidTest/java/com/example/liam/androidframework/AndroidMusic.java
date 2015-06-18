9ade86dc97fe0f8f488a2fc5ac582134b8ad4411
Liam Hackett <liamjhackett@gmail.com>
2015-06-18 23:23:16 +0100

    Add multiple audio options
    Added a Audio manager (AndroidAudio) to manage Sound (AndroidSound) and
    Music (AndroidMusic) within the application.

---
+++ app/src/androidTest/java/com/example/liam/androidframework/AndroidMusic.java
@@ -0,0 +1,16 @@
+package com.example.liam.androidframework;
+
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+
+/**
+ * Created by Liam on 18/06/2015.
+ */
+public class AndroidMusic {
+    private MediaPlayer mediaPlayer;
+    private boolean isPrepared;
+
+    public AndroidMusic(AssetFileDescriptor assetDescriptor){
+
+    }
+}
