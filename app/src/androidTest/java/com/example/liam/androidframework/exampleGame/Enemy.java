30cdd8f03424dfb3a8cee92b1087fd339f04fa1b
Liam Hackett <liamjhackett@gmail.com>
2015-06-21 15:45:21 +0100

    Add Game Code
    Add game code from Java-Platformer-Engine to project.

---
+++ app/src/androidTest/java/com/example/liam/androidframework/exampleGame/Enemy.java
@@ -0,0 +1,171 @@
+package com.example.liam.androidframework.exampleGame;
+
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+/**Superclass for managing the all enemy characters
+ * 
+ * @author Liam
+ *
+ */
+public class Enemy {
+
+	private int maxHealth, currentHealth, power, centerX, centerY, ifAttack;
+	private byte speedX, movementSpeed;
+	private ArrayList<Projectile> projectiles;
+	private Background bg;
+	private Rectangle hitbox; 
+	
+	
+	public Enemy(){
+		projectiles = new ArrayList<Projectile>();
+		ifAttack = 0;
+		centerX = 0;
+		centerY = 0;
+		maxHealth = 100; 
+		currentHealth = 100;
+		hitbox = new Rectangle(0,0,0,0);
+	}
+	
+	public void restart(){
+		maxHealth = 100; 
+		currentHealth = 100;
+	}
+	
+	/**Updates an enemy character this needs to happen before each render. 
+	 * 
+	 */
+	public void update(){
+		follow();
+		attack();
+		centerX += speedX;
+		speedX = (byte) (bg.getSpeedX()*5);
+		speedX = (byte) (bg.getSpeedX() * 5 + movementSpeed);
+		hitbox.setBounds(centerX-49 , centerY-49, 64, 64);
+		
+		if(hitbox.intersects(Player.getCheck())){
+			checkCollision();
+		}
+	}
+	
+	private void checkCollision(){
+		if(hitbox.intersects(Player.getBottom())|| hitbox.intersects(Player.getHead()) || hitbox.intersects(Player.getLeftHand()) || hitbox.intersects(Player.getRightHand())){
+			System.out.println("collision");
+		}
+	}
+	
+	public void follow() {
+		
+		if (centerX < -95 || centerX > 810) 
+			movementSpeed = 0;
+		else if (Math.abs(bootloader.getPlayer().getCenterX() - centerX) < 5)
+			movementSpeed = 0;
+		else {
+			
+			if(bootloader.getPlayer().getCenterX() >= centerX)
+				movementSpeed = 3;
+			else
+				movementSpeed = -2;
+		}
+		
+		
+	}
+	
+	public void die(){
+		
+	}
+	
+	public void attack(){
+		if(ifAttack == 50){
+			Projectile p = new Projectile(centerX + 0, centerY -25, 2);
+			p.setEnemy(true);
+			projectiles.add(p);
+			ifAttack = 0; 
+		}
+		ifAttack++;
+		
+	}
+	
+	public int getMaxHealth() {
+		return maxHealth;
+	}
+	
+	public void setMaxHealth(int maxHealth) {
+		this.maxHealth = maxHealth;
+	}
+	
+	public int getCurrentHealth() {
+		return currentHealth;
+	}
+	
+	public void setCurrentHealth(int currentHealth) {
+		this.currentHealth = currentHealth;
+	}
+	
+	public int getPower() {
+		return power;
+	}
+	
+	public void setPower(int power) {
+		this.power = power;
+	}
+	
+	public byte getSpeedX() {
+		return speedX;
+	}
+	
+	public void setSpeedX(byte speedX) {
+		this.speedX = speedX;
+	}
+	
+	public int getCenterX() {
+		return centerX;
+	}
+	
+	public void setCenterX(int centerX) {
+		this.centerX = centerX;
+	}
+	
+	public int getCenterY() {
+		return centerY;
+	}
+	
+	public void setCenterY(int centerY) {
+		this.centerY = centerY;
+	}
+	
+	public Background getBg() {
+		return bg;
+	}
+	
+	public void setBg(Background bg) {
+		this.bg = bg;
+	}
+	
+	public Rectangle getHitbox() {
+		return hitbox;
+	}
+
+
+	public void setHitbox(Rectangle hitbox) {
+		this.hitbox = hitbox;
+	}
+
+	public ArrayList<Projectile> getProjectiles() {
+		return projectiles;
+	}
+
+	public void setProjectiles(ArrayList<Projectile> projectiles) {
+		this.projectiles = projectiles;
+	}
+
+	@Override
+	public String toString() {
+		return "Enemy [maxHealth=" + maxHealth + ", currentHealth="
+				+ currentHealth + ", power=" + power + ", speedX=" + speedX
+				+ ", centerX=" + centerX + ", centerY=" + centerY + ", bg="
+				+ bg + "]";
+	}
+	
+	
+}
---
+++ app/src/androidTest/java/com/example/liam/androidframework/exampleGame/Projectile.java
@@ -0,0 +1,162 @@
+package com.example.liam.androidframework.exampleGame;
+
+import java.awt.Rectangle;
+/***
+ *  Used by the Player to attack the enemy.
+ * 
+ * @author Liam
+ *
+ */
+
+public class Projectile {
+
+	private int x, y, speedX,limit, direction; 
+	private boolean visible, enemy;
+	private Rectangle hitbox; 
+	
+	public Projectile(){
+		x = 0;
+		y = 0;
+		speedX = 7;
+		limit = 800;
+		visible = true;
+		
+		hitbox = new Rectangle(0, 0, 0, 0);
+	}
+	
+	public Projectile(int startX, int startY, int direction){
+		x = startX;
+		y = startY;
+		// 0= forward 1= behind 2= bellow 3= above 
+		this.direction = direction;
+		speedX = 7; 
+		limit = 800;
+		visible = true;
+		
+		hitbox = new Rectangle(0, 0, 0, 0);
+	}
+	
+	public Projectile(int startX, int startY, int direction, int speedX, int limit){
+		x = startX;
+		y = startY; 
+		// 0= forward 1= behind 2= bellow 3= above  
+		this.direction = direction;
+		this.speedX = speedX;
+		this.limit = limit;
+		
+		hitbox = new Rectangle(0, 0, 0, 0);
+	}
+	
+	public void update(){
+		if(direction == 0)
+			x += speedX;
+		else if(direction == 1)
+			x -= speedX;
+		else if (direction == 2)
+			y += speedX;
+		else if (direction == 3)
+			y -= speedX;
+		
+		hitbox.setBounds(x,y, 10, 5);
+		
+		if(x > limit){
+			visible = false;
+			hitbox = null;
+		}
+		
+		if(x < limit){
+			checkCollision();
+		}
+	}
+	
+	private void checkCollision(){
+		if(hitbox.intersects(bootloader.getFlyingEmemy().getHitbox()) && !enemy){
+			
+			Enemy en = bootloader.getFlyingEmemy();
+			visible = false;
+			
+			if(en.getCurrentHealth() > 0){
+				en.setCurrentHealth(en.getCurrentHealth()-25);
+			}
+			if(en.getCurrentHealth() == 0){
+				en.setCenterX(-100);
+				bootloader.setScore( bootloader.getScore()+5);
+			}
+			
+			bootloader.setScore((bootloader.getScore() + 1));
+		}
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getSpeedX() {
+		return speedX;
+	}
+
+	public void setSpeedX(int speedX) {
+		this.speedX = speedX;
+	}
+
+	public int getLimit() {
+		return limit;
+	}
+
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+
+	public boolean isVisible() {
+		return visible;
+	}
+
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+
+	public boolean isEnemy() {
+		return enemy;
+	}
+
+	public void setEnemy(boolean enemy) {
+		this.enemy = enemy;
+	}
+
+	public Rectangle getHitbox() {
+		return hitbox;
+	}
+
+	public void setHitbox(Rectangle hitbox) {
+		this.hitbox = hitbox;
+	}
+
+	public int getDirection() {
+		return direction;
+	}
+
+	public void setDirection(int direction) {
+		this.direction = direction;
+	}
+
+	@Override
+	public String toString() {
+		return "Projectile [x=" + x + ", y=" + y + ", speedX=" + speedX
+				+ ", limit=" + limit + ", visible=" + visible + "]";
+	}
+	
+	
+	
+}
