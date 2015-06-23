ed39168fb990092c5835aa2f1586945ea2c361f1
Liam Hackett <liamjhackett@gmail.com>
2015-06-23 01:52:47 +0100

    Bug fix
    Change image names to match disk titles.

--- app/src/main/java/com/example/liam/androidframework/exampleGame/LoadingScreen.java
+++ app/src/main/java/com/example/liam/androidframework/exampleGame/LoadingScreen.java
@@ -22,16 +22,16 @@ public class LoadingScreen extends Screen {
         Assets.setPlayer1(g.newImage("player.png", Graphics.ImageFormat.ARGB4444));
         Assets.setPlayer2(g.newImage("player2.png", Graphics.ImageFormat.ARGB4444));
         Assets.setPlayer3(g.newImage("player3.png", Graphics.ImageFormat.ARGB4444));
-        Assets.setPlayerJ(g.newImage("playerJ.png", Graphics.ImageFormat.ARGB4444));
-        Assets.setPlayerD(g.newImage("playerD.png", Graphics.ImageFormat.ARGB4444));
+        Assets.setPlayerJ(g.newImage("playerj.png", Graphics.ImageFormat.ARGB4444));
+        Assets.setPlayerD(g.newImage("playerd.png", Graphics.ImageFormat.ARGB4444));
 
         Assets.setEnemy1(g.newImage("enemy.png", Graphics.ImageFormat.ARGB4444));
         Assets.setEnemy2(g.newImage("enemy2.png", Graphics.ImageFormat.ARGB4444));
         Assets.setEnemy3(g.newImage("enemy3.png", Graphics.ImageFormat.ARGB4444));
 
-        Assets.setTileDirt(g.newImage("tile1.png", Graphics.ImageFormat.RGB565));
-        Assets.setTileGrassDirt(g.newImage("tile2.png", Graphics.ImageFormat.RGB565));
-        Assets.setTileWater(g.newImage("tile4.png", Graphics.ImageFormat.RGB565));
+        Assets.setTileDirt(g.newImage("Tile1.png", Graphics.ImageFormat.RGB565));
+        Assets.setTileGrassDirt(g.newImage("Tile2.png", Graphics.ImageFormat.RGB565));
+        Assets.setTileWater(g.newImage("Tile4.png", Graphics.ImageFormat.RGB565));
         Assets.setTileSpike(g.newImage("Spike.png", Graphics.ImageFormat.ARGB4444));
 
         game.setScreen(new MainMenuScreen(game));
